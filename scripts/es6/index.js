78f9a9465bf868beb9c9a9e95aa041a308ecede7
skripkalisa <mynamehere@yandex.ru>
2020-06-18 02:33:08 +0000

    eslint tweaks

--- scripts/es6/index.js
+++ scripts/es6/index.js
@@ -1,1 +1,17 @@
 console.clear()
+const array = ['apple', 'banana', 'orange']
+
+const breakfast = array.map((fruit) => `${fruit}s`)
+
+console.log(breakfast)
+function msgAfterTimeout(msg, who, timeout) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => resolve(`${msg} Hello ${who}!`), timeout)
+    reject()
+  })
+}
+msgAfterTimeout('', 'Foo', 100)
+  .then((msg) => msgAfterTimeout(msg, 'Bar', 200))
+  .then((msg) => {
+    console.log(`done after 300ms:${msg}`)
+  })
